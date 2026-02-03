683a3e3c13e979e2d42f1f4782c7a4aee7b807a3
321abcdef <moontongs37@gmail.com>
2026-02-03 19:21:02 +0800

    u

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -32,7 +32,7 @@ const Header = ({ toggleSidebar, userName = 'Dexter Mark B. Binongcal' }) => {
   const dateTimeStr = `${dateStr} - ${timeStr}`;
 
   return (
-    <header className="relative h-[72px] bg-white dark:bg-slate-900 border-b border-slate-200 dark:border-slate-800 flex items-center px-6 sticky top-0 z-10">
+    <header className="relative h-[89px] bg-white dark:bg-slate-900 border-b border-slate-200 dark:border-slate-800 flex items-center px-6 sticky top-0 z-10">
       
       {/* Left */}
       <div className="flex items-center gap-4">
